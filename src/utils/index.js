91a03a358c3dd35733f9b6f7f2366f2daa2662b5
张先龙 <907737948@qq.com>
2020-05-29 17:49:34 +0800

    5-29

--- src/utils/index.js
+++ src/utils/index.js
@@ -65,3 +65,51 @@ export async function httpHeader(func, errFunc) {
     return false;
   }
 }
+//函数节流
+const throttle = (fun, delay) => {
+  let last = null;
+  return () => {
+    const now = +new Date();
+    if (now - last > delay) {
+      fun();
+      last = now;
+    }
+  };
+};
+//函数防抖
+const debouce = (fun, delay) => {
+  let timer = null;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fun();
+    }, delay);
+  };
+};
+// 日期格式化
+export function formatDate(value, fmt) {
+  // var regPos = /^\d+(\.\d+)?$/;
+  // if (regPos.test(value)) {
+  //如果是数字
+  let getDate = new Date(value);
+  let opt = {
+    'Y+': getDate.getFullYear().toString(),
+    'M+': (getDate.getMonth() + 1).toString(),
+    'D+': getDate.getDate().toString(),
+    'h+': getDate.getHours().toString(),
+    'm+': getDate.getMinutes().toString(),
+    's+': getDate.getSeconds().toString(),
+  };
+  for (let k in opt) {
+    // if (new RegExp('(' + k + ')').test(fmt)) {
+    // 	console.log(opt[k]);
+    // 	console.log(RegExp.$1.length);
+    // 	fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? opt[k] : ('00' + opt[k]).substr(('' + opt[k]).length));
+    // }
+    let ret = new RegExp('(' + k + ')').exec(fmt);
+    if (ret) {
+      fmt = fmt.replace(ret[1], ret[1].length == 1 ? opt[k] : opt[k].padStart(ret[1].length, '0'));
+    }
+  }
+  return fmt;
+}
